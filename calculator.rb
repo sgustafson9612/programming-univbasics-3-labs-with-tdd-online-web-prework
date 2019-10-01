0ca663c953ac121d1c0d0ac71c6c61002414470d
samantha gustafson <sgustafson9612@gmail.com>
2019-10-01 01:59:19 +0000

    Done.

--- calculator.rb
+++ calculator.rb
@@ -1,1 +1,15 @@
-# Add your variables here
+first_number = get_variable_from_file('./calculator.rb',"first_number")
+expect(first_number).to be_an(Integer).or be_a(float)
+end
+expect(second_number).not_to equal(0)
+end.
+first_number + second_number = 
+
+
+
+
+
+
+
+
+
